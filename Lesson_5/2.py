f0cff0c1e5c97f7c04f29aec2152b2003c02d760
MaxRock32 <svk.90@bk.ru>
2019-03-23 03:43:23 +0300

    hw-5

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,22 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+class HexOperation:
+    def __init__(self, num_first, num_second):
+        self.num_first = num_first
+        self.num_second = num_second
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) + int(''.join(other.num_second), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) * int(''.join(other.num_second), 16)))[2:]
+
+
+hex_num_first = list(input('Введите первое шеснадцатиричное число: '))
+hex_num_second = list(input('Введите второе шеснадцатиричное число: '))
+
+res_sum = HexOperation(hex_num_first, hex_num_second) + HexOperation(hex_num_first, hex_num_second)
+res_mul = HexOperation(hex_num_first, hex_num_second) * HexOperation(hex_num_first, hex_num_second)
+print(f'Сумма чисел = {res_sum}\nПроизведение чисел = {res_mul}')
