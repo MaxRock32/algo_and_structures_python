3e6131f31fc738b6cd854407b3bb1f1a4bf1aa4e
MaxRock32 <svk.90@bk.ru>
2019-03-29 15:39:38 +0300

    lesson 7

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,41 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+from random import uniform
+
+def merge_sort(orig_list):
+    if len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+        return orig_list
+
+orig_list = [uniform(0, 50) for _ in range(10)]
+
+print(f"Исходный масив: {orig_list}")
+print(f"Отсортированный массив: {merge_sort(orig_list)}")
